d41aa317eb4cfd6f8e828802e2f76d9a97962cf6
Donovan McCray <domcrar@gmail.com>
2020-06-12 14:00:28 -0400

    Recreated the iOS app SettingsPage in SwiftUI. Have not yet added it to the existing view hierarchy

--- Merces/SettingsPage.swift
+++ Merces/SettingsPage.swift
@@ -6,8 +6,74 @@
 //  Copyright (c) 2015 DoMarsToyBox. All rights reserved.
 //
 
-import UIKit
+import SwiftUI
 import StoreKit
+
+struct SettingsRow: View {
+    @Binding var text: String
+    @Binding var isEnabled: Bool
+    
+    var body: some View {
+        Toggle(isOn: self.$isEnabled) {
+            Text(self.text)
+        }
+    }
+}
+
+struct Settings: View {
+    @EnvironmentObject var preferences: UserPreferences
+    @State private var isActive: Bool = false
+    
+    var body: some View {
+        Form {
+            Section(header: Text("General").font(.headline)) {
+//                NavigationLink(destination: MyMerces(), isActive: self.$isActive) {
+//                    Text("My Merces")
+//                }
+                SettingsRow(text: .constant("Tip Includes Tax"), isEnabled: self.$preferences.tipIncludeTax)
+                SettingsRow(text: .constant("Subtotal is Post Tax"), isEnabled: self.$preferences.subtotalIsPostTax)
+            }
+            
+            Section(header: Text("Round Up to Nearest Dollar").font(.headline)) {
+                SettingsRow(text: .constant("Tip Amount"), isEnabled: self.$preferences.roundTipAmount)
+                SettingsRow(text: .constant("Grand Total"), isEnabled: self.$preferences.roundTotalAmount)
+            }
+            
+            Section(header: Text("Accessibility").font(.headline)) {
+                SettingsRow(text: .constant("Use Your iPhone's Text Size"), isEnabled: self.$preferences.useDynamicText)
+            }
+            
+            Section(header: Text("Feedback").font(.headline)) {
+                Button(action: {
+                    
+                }) {
+                    Text("Leave a Rating")
+                        .foregroundColor(.primary)
+                        .onTapGesture {
+                            SKStoreReviewController.requestReview()
+                    }
+                }
+            }
+            
+            Section(header: Text("Support").font(.headline)) {
+                Text("Optimal Usage Guide")
+            }
+            
+            Section(header: Text("About").font(.headline)) {
+                Text("About Merces")
+            }
+        }
+        .background(Color.green)
+//        .foregroundColor(Color(coloringThemes.getBackgroundColor()))
+    }
+}
+
+struct SettingsPreview: PreviewProvider {
+    
+    static var previews: some View {
+        Settings().environmentObject(UserPreferences.sharedInstance)
+    }
+}
 
 class SettingsPage: UITableViewController {
 
