7bfa6bbd1bc7f895336472d0daefa3fe4fed469c
Donovan McCray <domcrar@gmail.com>
2020-06-15 09:05:48 -0400

    Created MainPageSubviews.swift to clear up space in MainPage

---
+++ TipTok/MainPageSubviews.swift
@@ -0,0 +1,166 @@
+//
+//  MainPageSubviews.swift
+//  TipTok
+//
+//  Created by Donovan McCray on 6/15/20.
+//  Copyright © 2020 DoMarsToyBox. All rights reserved.
+//
+
+import SwiftUI
+
+struct CurrencyView: View {
+    @Binding var value: Double
+    var body: some View {
+        Text(nForm.roundForCurrency(number: self.value))
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .padding()
+            .border(Color.primary, width: 2)
+            .cornerRadius(2)
+            .font(Font(UserPreferences.sharedInstance.checkForDynamicType(preferredFontSize: 18)))
+    }
+}
+
+struct IntegerView: View {
+    @Binding var value: Int
+    var body: some View {
+        Text(nForm.formatIntegerNumbers(self.value))
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .padding()
+            .border(Color.primary, width: 2)
+            .cornerRadius(2)
+            .font(Font(UserPreferences.sharedInstance.checkForDynamicType(preferredFontSize: 18)))
+    }
+}
+
+struct PercentageView: View {
+    @Binding var value: Double
+    var body: some View {
+        Text(nForm.roundForPercentWithTwoDecimalPlaces(self.value))
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .padding()
+            .border(Color.primary, width: 2)
+            .cornerRadius(2)
+            .font(Font(UserPreferences.sharedInstance.checkForDynamicType(preferredFontSize: 18)))
+    }
+}
+
+struct MainPageTopSubview: View {
+    @ObservedObject var calcModel: CalculationsModel = varAmts.calcModel
+    var body: some View {
+        GeometryReader { geo in
+            VStack {
+                VStack {
+                    Text("Subtotal")
+                        .font(Font(UserPreferences.sharedInstance.checkForDynamicType(preferredFontSize: 18)))
+                    CurrencyView(value: self.$calcModel.subtotal)
+                    
+                }
+                
+                HStack {
+                    VStack {
+                        Text("Sales Tax")
+                            .font(Font(UserPreferences.sharedInstance.checkForDynamicType(preferredFontSize: 18)))
+                        CurrencyView(value: self.$calcModel.taxAmount)
+                    }
+                    
+                    VStack {
+                        Text("Party of")
+                            .font(Font(UserPreferences.sharedInstance.checkForDynamicType(preferredFontSize: 18)))
+                        IntegerView(value: self.$calcModel.partySize)
+                    }
+                }.padding(.top)
+            }
+            .padding()
+            .frame(width: geo.size.width, height: geo.size.height)
+            .border(Color.primary, width: 2)
+            .cornerRadius(2)
+        }
+    }
+}
+
+struct MainPageMiddleSubview: View {
+    @ObservedObject var calcModel: CalculationsModel = varAmts.calcModel
+    var body: some View {
+        GeometryReader { geo in
+            VStack {
+                HStack {
+                    VStack {
+                        Text("Venue")
+                            .font(Font(UserPreferences.sharedInstance.checkForDynamicType(preferredFontSize: 18)))
+                        
+//                        VenuePicker()
+                        
+                        Text(self.calcModel.selectedVenue.name)
+                            .padding()
+                            .border(Color.primary, width: 2)
+                            .cornerRadius(2)
+                            .font(Font(UserPreferences.sharedInstance.checkForDynamicType(preferredFontSize: 18)))
+                    }
+                    
+                    VStack {
+                        Text("Tip %")
+                            .font(Font(UserPreferences.sharedInstance.checkForDynamicType(preferredFontSize: 18)))
+                        PercentageView(value: self.$calcModel.tipRate)
+                    }
+                }
+                
+                VStack {
+                    Text("Service Level")
+                        .font(Font(UserPreferences.sharedInstance.checkForDynamicType(preferredFontSize: 18)))
+                    ServiceQualityPicker()
+                }.padding(.top)
+            }
+            .padding()
+            .frame(width: geo.size.width, height: geo.size.height)
+            .border(Color.primary, width: 2)
+            .cornerRadius(2)
+        }
+    }
+}
+
+struct MainPageBottomSubview: View {
+    @ObservedObject var calcModel: CalculationsModel = varAmts.calcModel
+    var body: some View {
+        GeometryReader { geo in
+            VStack {
+                HStack(alignment: .center) {
+                    Text("Totaled Amounts")
+                        .font(Font(UserPreferences.sharedInstance.checkForDynamicType(preferredFontSize: 18)))
+                }
+                
+                HStack {
+                    Text("Tip Amount:")
+                        .font(Font(UserPreferences.sharedInstance.checkForDynamicType(preferredFontSize: 18)))
+                    CurrencyView(value: self.$calcModel.tipAmount)
+                }
+//                .padding(.top)
+                
+                HStack {
+                    Text("Grand Total:")
+                        .font(Font(UserPreferences.sharedInstance.checkForDynamicType(preferredFontSize: 18)))
+                    CurrencyView(value: self.$calcModel.totalAmount)
+                }
+//                .padding(.top)
+                
+                if self.calcModel.partySize != 1 {
+                    HStack {
+                        Text("Each Person:")
+                            .font(Font(UserPreferences.sharedInstance.checkForDynamicType(preferredFontSize: 18)))
+                        CurrencyView(value: self.$calcModel.totalAmountPerPerson)
+                    }
+//                    .padding(.top)
+                }
+            }
+            .padding()
+            .frame(width: geo.size.width, height: geo.size.height)
+            .border(Color.primary, width: 2)
+            .cornerRadius(2)
+        }
+    }
+}
+
+struct MainPageSubviews_Previews: PreviewProvider {
+    static var previews: some View {
+        MainPageTopSubview()
+    }
+}
