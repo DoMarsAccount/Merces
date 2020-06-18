1cc9601b24ac820265d3e553b4523032b0fdc356
Donovan McCray <domcrar@gmail.com>
2020-06-17 21:45:48 -0400

    Went a tad bit overboard with the coloring, will tone it back later.

--- TipTok/TextFields.swift
+++ TipTok/TextFields.swift
@@ -33,12 +33,38 @@ struct PercentageView: View {
 }
 
 struct TextFieldViewModifier: ViewModifier {
+    @Environment(\.colorScheme) var colorScheme
     func body(content: Content) -> some View {
         content
         .frame(minWidth: 0, maxWidth: .infinity)
         .padding()
+        .background(
+            ZStack {
+                Color(self.colorScheme == .dark ? "Eerie" : "Snow")
+                
+                RoundedRectangle(cornerRadius: 16, style: .circular)
+                    .foregroundColor(self.colorScheme == .dark ? .secondary : .secondary)
+                    .blur(radius: 4)
+                    .offset(x: -8, y: -8)
+                
+                RoundedRectangle(cornerRadius: 16, style: .continuous)
+                    .foregroundColor(self.colorScheme == .dark ? Color("Eerie") : Color("BabyPowder"))
+                    .padding(2)
+                    .blur(radius: 2)
+                
+//                RoundedRectangle(cornerRadius: 16, style: .continuous)
+//                .fill(
+//                    LinearGradient(gradient: Gradient(colors: [Color("BabyPowder"), .white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+//                )
+//                .padding(2)
+//                .blur(radius: 2)
+                    
+            }
+        )
         .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
-        .border(Color.primary, width: 2)
+//        .border(Color.primary, width: 2)
+        .shadow(color: self.colorScheme == .dark ? Color("Eerie") : Color("Alabaster"), radius: 2, x: 2, y: 2)
+        .shadow(color: self.colorScheme == .dark ? Color("Eerie") : Color("Alabaster"), radius: 2, x: -2, y: -2)
         .font(Font(UserPreferences.sharedInstance.checkForDynamicType(preferredFontSize: 24)))
     }
 }
