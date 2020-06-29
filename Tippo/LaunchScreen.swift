481411b67b623c5d95a7724bcbf4b9c3ed27bf09
Donovan McCray <domcrar@gmail.com>
2020-06-29 12:58:15 -0500

    (Finally) Deleted the unnecessary GlyphIcons from project assets

---
+++ Tippo/LaunchScreen.swift
@@ -0,0 +1,21 @@
+//
+//  LaunchScreen.swift
+//  Tippo
+//
+//  Created by Donovan McCray on 6/29/20.
+//  Copyright © 2020 DoMarsToyBox. All rights reserved.
+//
+
+import SwiftUI
+
+struct LaunchScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LaunchScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchScreen()
+    }
+}
