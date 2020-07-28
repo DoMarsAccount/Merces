5ef23ec9195b6d72d46679319aa553a298dc2235
Donovan McCray <domcrar@gmail.com>
2020-07-28 12:31:31 -0500

    Removed setup alert from watchOS app to promote the same discoverability as in iOS app

--- Tippo_watchOS WatchKit Extension/ContentView.swift
+++ Tippo_watchOS WatchKit Extension/ContentView.swift
@@ -9,10 +9,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isActive: Bool = false
     @ObservedObject var wCalcModel: CalculationsModel = CalculationsModel.sharedInstance
     @ObservedObject var userPrefs: UserPreferences = UserPreferences.sharedInstance
-    @State private var doesUserWantSetup: Bool = false
     
     var body: some View {
         ValuesView()
@@ -22,10 +20,6 @@ struct ContentView: View {
             .onAppear {
                 self.wCalcModel.tipRate = Tipping.sharedInstance.currentTipRate(for: self.wCalcModel.selectedVenue, service: self.wCalcModel.service)
                 _ = self.wCalcModel.computeTippingValues()
-            }
-            .modifier(SetupAlert(isActive: self.$userPrefs.shouldShowSetupAlert, doesUserWantSetup: self.$doesUserWantSetup))
-            .sheet(isPresented: self.$doesUserWantSetup) {
-                LocalSalesTaxPage().environmentObject(self.userPrefs)
             }
     }
 }
